9cbf1b66b6fd8357619b45d626c4cdf0721c76ac
Серафим <seraphim.uhanov@gmail.com>
2023-07-02 11:35:23 +0300

    Initial commit

---
+++ src/main/java/dev/uhanov/example/javasuggestion/JavaSuggestionApplication.java
@@ -0,0 +1,13 @@
+package dev.uhanov.example.javasuggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSuggestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/uhanov/example/javasuggestion/JavaSuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uhanov.example.javasuggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSuggestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
